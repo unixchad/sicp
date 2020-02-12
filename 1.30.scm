6111ff2b786bf8f83b4c2fcf48f798b5c1101ba0
Kyle Pollock <kyle.pollock7@gmail.com>
2020-02-11 21:19:39 -0600

    1.30

---
+++ 1.30.scm
@@ -0,0 +1,20 @@
+; (define (sum term a next b)
+;   (if (> a b)
+;     0
+;     (+ (term a)
+;        (sum term (next a) next b))))
+
+(define (sum-iter term a next b)
+  (define (iter a result)
+    (if (> a b)
+      result
+      (iter (next a) (+ result (term a)))))
+  (iter a 0))
+
+(define (integral f a b dx)
+  (define (add-dx x)
+    (+ x dx))
+  (* (sum-iter f (+ a (/ dx 2.0)) add-dx b)
+     dx))
+
+(integral cube 0 1 0.001)
