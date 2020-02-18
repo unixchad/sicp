710b7c5bd5ec8c09a50819470cc65dfbffdfb729
Kyle Pollock <kyle.pollock7@gmail.com>
2020-02-17 18:59:28 -0600

    1.37b

--- 1.37.scm
+++ 1.37.scm
@@ -1,8 +1,18 @@
+; (define (cont-frac n d k)
+;   (if (= k 0)
+;     (/ (n k) (d k))
+;     (/ (n k) (+ (d k) (cont-frac n d (- k 1))))))
+
+; (cont-frac (lambda (i) 1.0)
+; 	   (lambda (i) 1.0)
+; 	   10)
+
 (define (cont-frac n d k)
-  (if (= k 0)
-    (/ (n k) (d k))
-    (/ (n k) (+ (d k) (cont-frac n d (- k 1))))))
+  (define (iter result k)
+    (if (= k 0) result
+      (iter (/ (n k) (+ result (d k))) (- k 1))))
+  (iter 0 k))
 
 (cont-frac (lambda (i) 1.0)
 	   (lambda (i) 1.0)
-	   10)
+	   11)
