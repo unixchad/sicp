868b4191d64b2de26bb5d3ccf75ee87a3680b721
Kyle Pollock <kyle.pollock7@gmail.com>
2020-02-18 21:05:04 -0600

    2.2

---
+++ ch2/2.2.scm
@@ -0,0 +1,30 @@
+(define (print-point p)
+  (newline)
+  (display "(")
+  (display (x-point p))
+  (display ",")
+  (display (y-point p))
+  (display ")"))
+
+(define (make-point x y) (cons x y))
+(define (x-point p) (car p))
+(define (y-point p) (cdr p))
+
+(define (make-segment p1 p2)
+  (cons p1 p2))
+(define (start-segment s) (car s))
+(define (end-segment s) (cdr s))
+
+(define (midpoint-segment s)
+  (make-point
+    (/ (+ (x-point (start-segment s)) (x-point (end-segment s)) 2))
+    (/ (+ (y-point (start-segment s)) (y-point (end-segment s)) 2))))
+
+(print-point
+  (midpoint-segment
+    (make-segment
+      (make-point 5 8)
+      (make-point 7 9)
+      )
+    )
+  )
